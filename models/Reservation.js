de2b1f07ad79f37e5ab9000ec3e0f474d19d6a38
dj3rry <davis.jerryjr@gmail.com>
2023-03-12 22:29:09 -0400

    pushing initial BE folder structure and models

---
+++ models/Reservation.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+const mongoose = require('mongoose');
+
+const reservationSchema = new Schema({
+  listing: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Listing',
+    required: true
+  },
+  guest: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  startDate: {
+    type: Date,
+    required: true
+  },
+  endDate: {
+    type: Date,
+    required: true
+  },
+  totalCost: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Reservation = model('Reservation', reservationSchema);
+
+module.exports = Reservation
