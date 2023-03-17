d3da2e36f28468ee7dedf4554bd02d5660825dee
rak10 <ahmadus10@icloud.com>
2023-03-17 16:39:00 +0500

    added contact.js

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,12 +1,77 @@
-import React from 'react'
+// import React from 'react'
 
-// Import CSS
+// // Import CSS
 
+
+// const Contact = () => {
+//   return (
+//     <div>Contact</div>
+//   )
+// }
+
+// export default Contact
+
+import React, { useState } from "react";
+import "../styles/Contact.css";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-export default Contact
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
+  };
+
+  return (
+    <div className="contact-form-container">
+      <div className="contact-info">
+        <h3>Contact Us</h3>
+        <p>Email: example@company.com</p>
+        <p>Phone: (123) 456-7890</p>
+      </div>
+      <form onSubmit={handleSubmit} className="ContactForm">
+        <div className="form-group">
+          <label htmlFor="name" className="ContactUsLabel">
+            Name:
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="ContactUsInput"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            className="ContactUsInput"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            value={message}
+            className="ContactUsInput"
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="SendMsgBtn">
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
