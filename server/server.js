097b01562b5cc2b57878db5ca0c53d14b0cb8245
dj3rry <davis.jerryjr@gmail.com>
2023-03-22 13:12:51 -0400

    updating package.json and server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 // Import the ApolloServer class
 const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
 
 // Import the two parts of a GraphQL schema
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require("./utils/auth");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -15,10 +16,16 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
